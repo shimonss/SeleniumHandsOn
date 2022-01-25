399f9c485d32b02a6dce657d0b3be742e2f4224b
shimonss <96369094+shimonss@users.noreply.github.com>
2022-01-25 14:20:29 +0200

    Update DriverSingleton.java

--- src/test/java/DriverSingleton.java
+++ src/test/java/DriverSingleton.java
@@ -1,15 +1,1 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DriverSingleton {
-    private static WebDriver driver;
-
-    public static WebDriver getDriverInstance(){
-        if(driver == null){
-            System.setProperty("WebDriver.chrome.driver", "C:\\Program Files (x86)\\chromedriver.exe");
-            driver = new ChromeDriver();
-        }
-
-        return driver;
-    }
-}
